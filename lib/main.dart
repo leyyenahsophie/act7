36d0f9e5cb569cfc0ff7a28acbf7963d420da89f
ewilliams153 <ewilliams153@student.gsu.edu>
2025-02-26 14:29:11 -0500

    add color picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,6 +21,15 @@ class FadingTextAnimation extends StatefulWidget {
 
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
   bool _isVisible = true;
+  Color pickerColor = Colors.black;
+  Color textColor = Color(0xff443a49);
+
+  void changeColor(Color color) {
+    setState(() {
+      pickerColor = color;
+      textColor = pickerColor;
+    });
+  }
 
   void toggleVisibility() {
     setState(() {
@@ -33,15 +43,26 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
       appBar: AppBar(
         title: Text('Fading Text Animation'),
       ),
-      body: Center(
-        child: AnimatedOpacity(
-          opacity: _isVisible ? 1.0 : 0.0,
-          duration: Duration(seconds: 1),
-          child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: AnimatedOpacity(
+              opacity: _isVisible ? 1.0 : 0.0,
+              duration: Duration(seconds: 1),
+              child: Text(
+                'Hello, Flutter!',
+                style: TextStyle(fontSize: 24, color: textColor),
+              ),
+            ),
           ),
-        ),
+          SizedBox(height: 20),
+          BlockPicker(
+          pickerColor: textColor, 
+          onColorChanged: changeColor,
+          ),
+        ],
+        
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: toggleVisibility,
